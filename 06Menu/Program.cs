52c94b13c5df98ded823c0a4ac91663c81491887
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2025-02-01 00:23:30 -0300

    Exercicio 06
    Estrutura case para menu console

---
+++ 06Menu/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Menu{
+    class Program {
+        static void Main(string[] args){
+            Console.WriteLine("01 - Criar novo usuario");
+            Console.WriteLine("02 - Editar Usuario");
+            Console.WriteLine("03 -Excluir usuario");
+            double opcaoSelecionada = double.Parse(Console.ReadLine());
+            
+            switch(opcaoSelecionada){
+                case  1:
+                Console.WriteLine("Informe o nome do usuario");
+                break;
+                case  2:
+                Console.WriteLine("Qual o usuario deseja editar");
+                break;
+                case  3: 
+                Console.WriteLine("Qual usuario deseja excluir");
+                break;
+                default:
+                Console.WriteLine("Informe uma condição valida");
+                break;
+            }
+        }
+    }
+}
