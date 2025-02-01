0dceb4c94efaaac1220c4c5a8482c02152028a6e
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2025-01-31 23:57:17 -0300

    Exercício cinco if else
    Desconto

---
+++ 05Desconto/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Desconto {
+    class Program {
+        static void Main(string[] args){
+            Console.Write("Valor da compra: R$ ");
+            float valorDaCompra = float.Parse(Console.ReadLine());
+
+            if(valorDaCompra >= 100){
+                float valorPagar =  valorDaCompra - (valorDaCompra*0.1f);
+                Console.WriteLine($"Valor da compra é de R$ {valorDaCompra}, com o desconto de 10% fica R$ {valorPagar}");
+            }else{
+                Console.WriteLine($"Valor da compra é de R$ {valorDaCompra}");
+            }
+        }
+    }
+}
